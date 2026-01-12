bea6bb46d0f566a986e31ecb2898ce5290bb7b6f
Ayush Gupta <ag6320226@gmail.com>
2026-01-12 20:47:08 +0530

    access

---
+++ virtual_func.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+using namespace std;
+
+// class Myfriend{
+//      public: 
+//     virtual void Myshoes()=0;
+// };
+// class Me:public Myfriend{
+//     public:
+//     void Myshoes()override{
+//         cout<<"These are my shoes now";
+//     }
+// };
+
+// int main(){
+//     Me m;
+//     m.Myshoes();
+//     return 0;
+// }
+
+// class Demo{
+//     private:
+
+//     int a=1;
+    
+//     protected:
+//     int b=2;
+
+//     public:
+//     int c=3;
+
+//     void show(){
+//         cout<< "a ="<< a<< ",b ="<<b<< ",c="<<c<<endl;
+//     }
+
+// };
+// class Derived : public Demo{
+//     public:
+//     void access(){
+//         cout<< b<< endl;
+//         cout<<c<<endl;
+
+//     }
+// };
+// int main(){
+//     Derived d;
+//     d.access();
+//     return 0;
+// }
